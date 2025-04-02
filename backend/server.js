210d7c947e2dd08cbdc665944385d351fc09ca62
YASH <yashsanjay.k21@iiits.in>
2025-04-03 00:25:41 +0530

    port changed

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === "production") {
 	});
 }
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
 	console.log(`Server running on port ${PORT}`);
 	connectDB();
 });
